8e4a3f77a9bfc52b39fbf9c5655348424d94754c
Laniel Park <Laniel8177@gmail.com>
2024-05-06 12:16:41 +0900

    feat : add img loader

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,25 @@
-import './app.scss';
+import './app.scss'
 
 function main(): void {
     const button = document.querySelector("#testButton")
     button?.addEventListener("click", testSubmitHandler)
+
+    testImageLoader()
 }
 
 function testSubmitHandler(e: Event): void {
     e.preventDefault();
     const outputElement = document.querySelector('#output')
     outputElement.textContent = 'Scripts work!'
+}
+
+function testImageLoader() {
+    const image = document.createElement("img")
+    image.src = require("../assets/robot.svg")
+    image.style.width = "200px"
+
+    const imageWrapper = document.getElementById("imageTest")
+    imageWrapper.appendChild(image)
 }
 
 main()
--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,13 @@ module.exports = {
         test: /\.scss$/,
         use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][ext]'
+        }
+      },
     ],
   },
   resolve: {
